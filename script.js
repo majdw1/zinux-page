3dca73ea6ae92900ef1cea7e162adc7a5007a1e9
majdw1 <majdw12345@gmail.com>
2025-11-22 04:22:10 +0200

    a

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Mobile nav toggle
+
 document.getElementById('navBtn').onclick = () =>{
   const m=document.getElementById('navMobile');
   m.style.display = m.style.display==='flex' ? 'none' : 'flex';
 };
 
-// Counter animation
 let target=120, n=0;
 let counter=setInterval(()=>{
   n++; document.getElementById('count').innerText=n;
